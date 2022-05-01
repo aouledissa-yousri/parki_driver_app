f65ac2f1880e05db952f1d43c04c13f319507d92
aouledissa <aouledissayousri@gmail.com>
2022-05-02 00:38:26 +0100

    added loading screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { LandingComponent } from './pages/offline/components/landing/landing.com
 import { LoginComponent } from './pages/offline/components/login/login.component';
 import { SignUpComponent } from './pages/offline/components/sign-up/sign-up.component';
 import { HomeComponent } from './pages/online/components/home/home.component';
-import { LoadingComponent } from './common-components/loading/loading.component';
 
 const routes: Routes = [
   {path: "",  children: [
